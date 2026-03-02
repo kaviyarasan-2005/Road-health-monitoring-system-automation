283c3b70d55915136d3b86d02bbcb6cba4414818
Kaviyarasan <kavigunasekar185@gmail.com>
2026-03-02 11:46:40 +0530

    public model base compleated for image

--- Website/frontend/pages/user/script/publicsScript.js
+++ Website/frontend/pages/user/script/publicsScript.js
@@ -2,8 +2,9 @@ const form = document.getElementById("reportForm");
 const message = document.getElementById("message");
 
 form.addEventListener("submit", async function (e) {
+ console.log(e+"  sjhdfjs");
     e.preventDefault();
-
+    console.log(e+"  sjhdfjs");
     const fileInput = document.getElementById("imageInput");
     const file = fileInput.files[0];
 
@@ -11,14 +12,20 @@ form.addEventListener("submit", async function (e) {
     formData.append("image", file);
 
     message.innerText = "Analyzing image...";
-
     try {
-        const response = await fetch("http://127.0.0.1:5000/predict", {
-            method: "POST",
-            body: formData
-        });
+       const response = await fetch("http://127.0.0.1:5000/api/public/detect", {
+    method: "POST",
+    body: formData
+});
 
-        const data = await response.json();
+console.log("Response status:", response.status);
+
+const text = await response.text();
+console.log("Raw response:", text);
+
+const data = JSON.parse(text);
+console.log("Parsed data:", data);
+        // const data = await response.json();
 
         message.style.color = "green";
         message.innerText = 
